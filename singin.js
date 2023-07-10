9b2a383715a99e2881168965535859c5ad5754d9
junaidsalam639 <junaidsalam639@gmail.com>
2023-07-10 23:14:16 +0500

    'first'

--- singin.js
+++ singin.js
@@ -1,7 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-  import { getAuth , signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-
+  import { getAuth , signInWithEmailAndPassword , GoogleAuthProvider ,signInWithPopup } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+  
   const firebaseConfig = {
     apiKey: "AIzaSyDnQAk5ONtVrlVukJNA1ah4dt0kZVCpzBk",
     authDomain: "save-be155.firebaseapp.com",
@@ -15,6 +15,7 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 document.getElementById("btn").addEventListener('click', () => {
      let email = document.getElementById("email").value;
      let password = document.getElementById("password").value;
@@ -35,5 +36,32 @@ document.getElementById("btn").addEventListener('click', () => {
         alert("sing up is first")
         window.location.href = './singup.html'
     });
+})
+
+// SING IN WITH GOOGLE
+document.getElementById("google").addEventListener('click' , (e) => {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+    alert("SING IN SUCCESSFULLY");
+        window.location.href = './quiz.html'
+   
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  
 })
 
